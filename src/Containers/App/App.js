aaf494c74a9b57dfc4698ec6f58cfe5b1423e8c7
Maysam <maysam82.dev@gmail.com>
2020-03-24 23:48:38 +1100

    add test for click simulation

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
 	constructor() {
@@ -7,10 +8,20 @@ class App extends Component {
 			items: [],
 		};
 	}
+	addNewItem = () => {
+		const { items } = { ...this.state };
+		const ids = items.map(({ id }) => id && id);
+		const maximum_id = ids.length > 0 ? Math.max(...ids) : 0;
+		items.push({ id: maximum_id + 1 });
+		this.setState({ items });
+	};
 	render() {
 		return (
 			<div>
 				<header>Sample Test Driven Development Project</header>
+				<Button className="btn-add" onClick={this.addNewItem}>
+					Add New Item
+				</Button>
 			</div>
 		);
 	}
--- src/Containers/App/App.test.js
+++ src/Containers/App/App.test.js
@@ -10,4 +10,8 @@ describe('App component', () => {
 	it('initializes the `state` with an empty list of items', () => {
 		expect(app.state().items).toEqual([]);
 	});
+	it('adds a new item to `state` items array while clicking the `add items` button', () => {
+		app.find('.btn-add').simulate('click');
+		expect(app.state().items).toEqual([{ id: 1 }]);
+	});
 });
